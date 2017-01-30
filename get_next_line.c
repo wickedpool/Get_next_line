6d2b5d0c7d4d4bd6b6e6972ac66c0009732943af
Wickedpool <thgiraud@student.42.fr>
2017-01-30 15:36:32 +0100

    New

--- get_next_line.c
+++ get_next_line.c
@@ -5,60 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: thgiraud <thgiraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/04 15:10:15 by thgiraud          #+#    #+#             */
-/*   Updated: 2017/01/04 15:10:55 by thgiraud         ###   ########.fr       */
+/*   Created: 2017/01/30 08:42:48 by thgiraud          #+#    #+#             */
+/*   Updated: 2017/01/30 15:35:52 by thgiraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int			read_line(int fd, char **buff)
+{
+	char	tab[BUFF_SIZE + 1];
+	int		ret;
+
+	ret = 1;
+	while (ft_strstr(*buff, ENDL) == NULL && ret != 0)
+	{
+		if (ret = read(fd, tab, BUFF_SIZE) == -1)
+			return (-1);
+		tab[ret] = END;
+		*buff = ft_strjoin(*buff, tab);
+		ft_memset(*buff, 0, ret);
+	}
+	return (ret);
+}
+
 int			get_next_line(int const fd, char **line)
 {
-	static char		*tmp = NULL;
-	char			*tab;
-	char			*buff;
-	int				ret;
+	static char *buff;
+	char		*tmp;
+	int			ret;
 
+	if (BUFF_SIZE <= 0 || fd <= 0)
+		return (-1);
+	if (!(buff))
+		buff = ft_memalloc(BUFF_SIZE);
+	ret = read_line(fd, &buff);
 
-	printf("--------Bienvenue dans gnl\n");
-	if (tab == NULL)
-		tab = ft_memalloc(BUFF_SIZE);
-	printf("dans gnl\n");
-	tmp = ft_strncpy(ft_memalloc(BUFF_SIZE), tab, BUFF_SIZE);
-	buff = ft_strchr(tmp, ENDL);
-	while (!(buff))
-	{
-		printf("----on est dans le while\n");
-		ret = read(fd, tmp, BUFF_SIZE);
-		if (ret < 0)
-			return (-1);
-		printf("TMP == %s\n", tmp);
-		 if (ret == 0)
-		{
-			if ((buff = ft_strchr(tmp, END)) == tmp)
-				return (0);
-		}
-		else
-		{
-			tmp[ret] = END;
-			printf("TMP si ret > 0 et tmp[ret] = END  ==%s\n", tmp);
-			buff = ft_strchr(tmp, ENDL);
-			printf("BUFF ==%s\n", buff);
-			printf("TMP si ret > 0 apres strchr ==%s\n", tmp);
-			tab = ft_strjoin(tmp, tab);
-			printf("TMP ==%s\n", tmp);
-			printf("TAB ==%s\n", tmp);
-		}
-		printf("on sort du while");
-		tab[ft_strchr(tab, ENDL) - tab] = END;
-		printf("TAB ==%s\n", tmp);
-		*line = ft_strdup(tab);
-		printf("LINE ==%s\n", *line);
-		ft_memmove(tab, ft_strchr(tab, ENDL) + 1, ft_strlen(ft_strchr(tab, ENDL)) + 1);
-		printf("TAB FIN ==%s\n", tmp);
-		return (1);
-		free(tmp);
-		free(tab);
-	}
-	return (-1);
 }
