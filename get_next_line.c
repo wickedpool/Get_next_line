72e6d69e61c0e2f054890820a8eb292e32e949c7
Wickedpool <thgiraud@student.42.fr>
2017-02-01 14:39:08 +0100

    addREAD-Verif

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thgiraud <thgiraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/30 08:42:48 by thgiraud          #+#    #+#             */
-/*   Updated: 2017/01/31 18:18:05 by thgiraud         ###   ########.fr       */
+/*   Updated: 2017/02/01 14:38:38 by thgiraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,38 @@ int			read_line(int fd, char **buff)
 	char	*tab;
 	int		ret;
 
-	printf("WELCOME READ LINE\n");
 	ret = 1;
 	while (!ft_strchr(*buff, ENDL))
 	{
-		printf("On rentre dans while READ\n");
 		ret = read(fd, tab, BUFF_SIZE);
 		tab[ret] = END;
-		printf("TAB : %s\n", tab);
 		*buff = ft_strjoin(*buff, tab);
-		printf("BUFF APRES JOINS : %s\n", *buff);
 	}
 	return (ret);
+}
+
+static int	ft_verif(char **buff, char **tab, char **line)
+{
+	
 }
 
 int			get_next_line(int const fd, char **line)
 {
 	static char *buff;
 	char		*tmp;
+	int			result;
 	int			ret;
 
-	printf("WELCOME TO GNL\n");
-	if (BUFF_SIZE <= 0 || fd <= 0)
+	tmp = ft_strnew(BUFF_SIZE);
+	if (BUFF_SIZE <= 0 || fd < 0 || (ret = read(fd, tmp, 0)) < 0)
 		return (-1);
-	printf("GESTION D'ERREUR\n");
-	if (!buff) //MALLOC BUFF
+	while ((ret = read(fd, tmp, BUFF_SIZE)) > 0)
 	{
-		printf("ON ALLOUE BUFF\n");
-		buff = ft_memalloc(BUFF_SIZE);
-		buff[0] = 0;
-		printf("BUFF : %s\n", buff);
+		result = ft_verif(&buff[fd], &tab, line);
+		free(tmp);
 	}
-	if (ret < 0)
-		return (-1)
 	else if (ret == 0) // LET'S READ
 		ret = read_line(fd, &buff); //HEADING TO READLINE
-	printf("ret : %d\n", ret);
 	if (*line)
 		ret = 1;
 	return (ret);
